ca1308842fac2a2046e9a6ea23352a308702e7af
Adithya P <112391823+adityaajf@users.noreply.github.com>
2024-05-29 13:09:00 +0530

    Add files via upload
    It is python program works of GEMINI LLM model. Deployed using Streamlit, It takes the user input as image and gives the prompt of what is present in the image data.

---
+++ Streamlit_image_app.py
@@ -0,0 +1,29 @@
+
+######## Import the required pacakges ############
+import streamlit as st
+import google.generativeai as genai
+from PIL import Image
+
+####### Provide your api key ###################
+api_key="AIzaSyBUUejpbU_3eouCT96lkHmkE2mQ6jQb8TU"
+genai.configure(api_key=api_key)
+
+######## Choose the heading ###############
+st.header("Image analytics")
+
+########## Upload a file ##################
+uploaded_file=st.file_uploader("Upload an image",type=["png","jpg","jpeg"])
+
+if uploaded_file is not None:
+    st.image(Image.open(uploaded_file))
+
+############ What you want ask ####################
+prompt=st.text_input("Enter the text")
+
+######### Use genai skill ##########################
+
+if st.button("GET RESPONSE"):
+    img=Image.open(uploaded_file)
+    model=genai.GenerativeModel("gemini-1.0-pro-vision-latest")
+    response=model.generate_content([prompt,img])
+    st.markdown(response.text)
